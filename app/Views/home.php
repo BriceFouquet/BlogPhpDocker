dd005676b1e0167a8d3e0f01d44c3df70d5c7c26
clemzouuu <clemkinoa@gmal.com>
2022-11-27 15:18:43 +0100

    commit

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,8 +1,12 @@
-<?php /** @var App\Entity\Users $users */ ?>
+<?php /** @var App\Entity\User $user */ ?>
+    <h1><?= $phrase; ?></h1>
 
+<?= $machin ?>
 
 <?php
-/** @var App\Entity\Posts[] $posts */
+/** @var App\Entity\Post[] $posts */
 foreach ($posts as $post) {
     echo $post->getContent();
 }
+
+
--- app/index.php
+++ app/index.php
@@ -1,19 +1,59 @@
 <?php
+
+use App\Route\Route;
+
 require_once 'vendor/autoload.php';
 
-$Clement = new \App\Manager\UserManager(new \App\Factory\PDOFactory());
+// Stocke l'emplacement du controller
+$controllerDir = dirname(__FILE__) . '/src/Controller';
 
-?>
-<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blog</title>
-</head>
-     <body>
+// retourne un tableau de tous les fichiers dans controllerDIR
+$dirs = scandir($controllerDir);
 
-     </body>
-</html>
+$controllers = [];
+
+// Met les routes dans controllers
+foreach ($dirs as $dir) {
+    if ($dir === "." || $dir === "..") {
+        continue;
+    }
+
+    $controllers[] = "App\\Controller\\" . pathinfo($controllerDir . DIRECTORY_SEPARATOR . $dir)['filename'];
+}
+
+
+$routesObj = [];
+
+foreach ($controllers as $controller) {
+    $reflection = new ReflectionClass($controller);
+
+    foreach ($reflection->getMethods() as $method) {
+        foreach ($method->getAttributes() as $attribute) {
+            /** @var Route $route */
+            $route = $attribute->newInstance();
+            $route->setController($controller)
+                ->setAction($method->getName());
+
+            $routesObj[] = $route;
+        }
+    }
+}
+
+$url = "/" . trim(explode("?", $_SERVER['REQUEST_URI'])[0], "/");
+
+foreach ($routesObj as $route) {
+    if (!$route->match($url) || !in_array($_SERVER['REQUEST_METHOD'], $route->getMethods())) {
+        continue;
+    }
+
+    $controlerClassName = $route->getController();
+    $action = $route->getAction();
+    $params = $route->mergeParams($url);
+
+    new $controlerClassName($action, $params);
+    exit();
+}
+
+echo "NO MATCH";
+
+die;
